ef73d574ac32182b25f02632ad54d0c721021a89
gabriel254 <12846878+gabriel254@users.noreply.github.com>
2021-04-23 05:03:14 -0400

    按顺序上传

--- Uploader.py
+++ Uploader.py
@@ -71,7 +71,7 @@ class Uploader(BiliLive):
                 output_parts = []
                 datestr = global_start.strftime(
                     '%Y{y}%m{m}%d{d}').format(y='年', m='月', d='日')
-                filelists = os.listdir(self.output_dir)
+                filelists = sorted(os.listdir(self.output_dir))
                 for filename in filelists:
                     if os.path.getsize(os.path.join(self.output_dir, filename)) < 1024*1024:
                         continue
